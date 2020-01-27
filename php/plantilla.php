776f61b14b4eba83b54498a40256a96750552e6b
Leo <35300461+frexel@users.noreply.github.com>
2020-01-27 10:48:40 -0300

    nueva plantilla

--- php/plantilla.php
+++ php/plantilla.php
@@ -6,66 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="../css/all.min.css">
-    <link rel="stylesheet" href="../css/lailaStyle.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/brook.css">
+    <link rel="stylesheet" href="../css/lailaStyle.css">
     <title>Lila Frank</title>
 </head>
 
-<body style="max-width: 100vw;">
-    <div class="container-fluid">
+<body>
+    <!-- Wrapper -->
+    <div id="wrapper" class="wrapper">
+        <?php require_once('../partials/navFront.php'); ?>
 
-        <!-- NAVBAR FULLSCREEN -->
-        <!-- <div id="nav" class="container-fluid noPad contenedorAside h-100">
-            <div class="row">
-                <div class="col-xs-10 col-md-8 alto fondoCrema">
-                    <div class="container">
-                        <div class="row d-flex justify-content-end">
-                            <div class="col-2 p-3 d-flex justify-content-end">
-                                <div class="wrapper">
-                                    <a class="close-button" onclick="closeNav()">
-                                        <div class="in">
-                                            <div class="close-button-block"></div>
-                                            <div class="close-button-block"></div>
-                                        </div>
-                                        <div class="out">
-                                            <div class="close-button-block"></div>
-                                            <div class="close-button-block"></div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <?php require_once('../partials/toolbarFront.php'); ?>
+
+        <!-- MAIN CONTENT -->
+        <main class="page-content ml--100 ml_lg--0 ml_md--0 ml_sm--0">
+            <!-- Contenido Principal -->
+            <div class="container-fluid">
+                <?php require_once('../partials/advert.php'); ?>
+               
+               
+                <!-- CONTENIDO PRINCIPAL -->
+                <div class="row">
+                    
                 </div>
-                <div id="backnav" class="col-sx-2 col-md-4 h100 noPad" href="javascript:void(0)" onclick="closeNav()"></div>
+
+
+
+                <?php require_once('../partials/footer.php'); ?>
             </div>
-        </div> -->
-
-
-        <div class="row">
-
-            <?php require_once('../partials/nav.php'); ?>
-
-            <div class="offset-1 col-11 main-content noPad">
-                <div class="container-fluid">
-                    <!-- Contenido Principal -->
-                    <?php require_once('../partials/advert.php'); ?>
-
-
-
-
-                    <?php require_once('../partials/footer.php'); ?>
-                </div>
-            </div>
-        </div>
+        </main>
     </div>
 
 
+    <!-- <script src="js/modernizr-3.6.0.min.js"></script> -->
     <script src="../js/jquery-3.4.1.min.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
-    <script src="../js/navbar.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/main.js"></script>
     <script src="../js/home.js"></script>
     <script src="../js/jquery.instagramFeed.min.js"></script>
 
